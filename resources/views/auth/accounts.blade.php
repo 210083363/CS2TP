5a2e514e4d3bcf206519e5086b04953c6bbe438d
dylan <goesdylan@gmail.com>
2022-11-29 22:02:36 +0000

    view all accounts for admins

---
+++ resources/views/auth/accounts.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('title', 'Welcome')
+
+@section('content')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">name</th>
+      <th scope="col">email</th>
+      <th scope="col">group</th>
+      <th scope="col">join_date</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse($data as $user)
+    <tr>
+      <td>{{$user->id}}</td>
+      <td>{{$user->name}}</td>
+      <td>{{$user->email}}</td>
+      <td>{{$user->group}}</td>
+      <td>{{$user->created_at}}</td>
+    </tr>
+    @empty
+    <td>No products</td>
+    @endforelse
+  </tbody>
+</table>
+@endsection

be7a2f65eccdc3cf83031210084899c1a38f4c33
210083363 <210083363@aston.ac.uk>
2022-12-02 15:39:11 +0000

    order page frondend

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -4,43 +4,44 @@
 
 @section('content')
 
+
+<link rel="stylesheet" href="css/basket.css">
 @forelse($data as $order)
-<h4>OrderID: {{$order['id']}} </h4>
-<table class="table">
-
-  <thead class="thead-dark">
-    <tr>
-      @if ($user->group > 1) <th scope="col">User</th> @endif
-      <th scope="col">Product</th>
-      <th scope="col">Quantity</th>
-      <th scope="col">Price</th>
-
-    </tr>
-  </thead>
-  <tbody>
-
-    @foreach($order['sales'] as $sale)
-    <tr>
-      @if ($user->group > 1) <td>{{$order['user']->name}}</td> @endif
-      <td>{{$sale['name']}}</td>
-      <td>{{$sale['quantity']}}</td>
-      <td>{{$sale['price']}}</td>
-    </tr>
-    @endforeach
-    <tr>
-      @if ($user->group > 1) <td></td> @endif
-      <td></td>
-      <td></td>
-      <td><b>Total: {{$order['total_price']}}</b></td>
-    </tr>
-  </tbody>
-</table>
+<div class="container-log">
+     <div class="title"><h4>OrderID: {{$order['id']}} </h4></div>
+     <br>
+     <table class="fn_table">
+       <thead class="thead-dark">
+           <tr>
+               @if ($user->group > 1) <th scope="col">Name</th>@endif
+               <th scope="col">Product</th>
+               <th scope="col">Quantity</th>
+               <th scope="col">Price</th>
+           </tr>
+       </thead>
+       <tbody>
+           @php($total = 0)
+           @forelse($order['sales'] as $sale)
+           <tr>
+            @if ($user->group > 1) <td>{{$order['user']->name}}</td> @endif
+            <td>{{$sale['name']}}</td>
+            <td>{{$sale['quantity']}}</td>
+            <td>£{{$sale['price']}}</td>
+            </tr>
+           {{-- @php($total += $product['total'])  --}}
+           @empty
+               <td>No products</td>
+           @endforelse 
+           <tr>
+                @if ($user->group > 1) <td colspan='3'>{{$order['user']->name}}</td>
+                @else <td colspan='2'>Total</td>  @endif
+               <td>£{{$order['total_price']}}</td>
+           </tr>
+       </tbody>
+   </table>
+</div>
 @empty
 <td>No orders</td>
-@endforelse
-</tbody>
-</table>
-
-<a href="{{url('home')}}"><button>Home</button></a>
+@endforelse 
 
 @endsection
